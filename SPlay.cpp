8c4cfdec9d108c99cf8903b3ac074dd98308b90f
weisang <1428206861@qq.com>
2024-08-05 22:46:27 +0800

    界面仿照PotPlayer完善

--- SPlay.cpp
+++ SPlay.cpp
@@ -1,9 +1,11 @@
 #include "SPlay.h"
 #include "ui_SPlay.h"
 #include<QFile>
+#include<QFileDialog>
 SPlay::SPlay(QWidget *parent)
 	: QWidget(parent)
 	,ui(new Ui::SPlay)
+	,m_playListModel(new QStandardItemModel(this))
 {
 	ui->setupUi(this);
 	initUi();
@@ -25,13 +27,53 @@ void SPlay::initUi()
 	QString styleStr = file.readAll();
 	this->setStyleSheet(styleStr);
 
+	this->setWindowIcon(QIcon(":/Res/player.png"));
 
-	this->setWindowIcon(QIcon(":/Res/SPlay.png"));
+	//titleBar
+	ui->iconLab->setPixmap(QPixmap(":/Res/player.png").scaled(ui->iconLab->size()));
+	ui->closeBtn->setIcon(QIcon(":Res/close.png"));
+	ui->maxBtn->setIcon(QIcon(":Res/max.png"));
+	ui->minBtn->setIcon(QIcon(":Res/min.png"));
+	ui->topBtn->setIcon(QIcon(":Res/top_off.png"));
+
+	//controlWidget
 	ui->playBtn->setIcon(QIcon(":/Res/pause.png"));
+	ui->prevBtn->setIcon(QIcon(":/Res/prev.png"));
+	ui->nextBtn->setIcon(QIcon(":/Res/next.png"));
+	ui->playListBtn->setIcon(QIcon(":/Res/playlist.png"));
+	ui->volumeLab->setPixmap(QPixmap(":/Res/volume.png").scaled(ui->volumeLab->size()));
 
-	//ui->playSpeedCmb->setView(new QListView());
-	ui->playSpeedCmb->addItem(QIcon(":/Res/1.png"), "1 倍速", 1);
-	ui->playSpeedCmb->addItem(QIcon(":/Res/1.5.png"), "1.5 倍速", 1.5);
-	ui->playSpeedCmb->addItem(QIcon(":/Res/2.png"), "2 倍速", 2);
-	ui->playSpeedCmb->setCurrentIndex(0);//即第一项
+	//playListWidget
+	ui->playModeBtn->setIcon(QIcon(":/Res/order-play.png"));
+	ui->addPlayListBtn->setIcon(QIcon(":/Res/add.png"));
+	ui->clearPlayListBtn->setIcon(QIcon(":/Res/clear.png"));
+	ui->playListView->setModel(this->m_playListModel);
+	delegate = new CustomDelegate(ui->playListView);
+	//ui->playListView->setItemDelegate(delegate);
+	ui->playListView->setEditTriggers(QListView::NoEditTriggers);
+	auto item = new QStandardItem();
+	item->setEnabled(false);
+	item->setSelectable(false);
+	item->setFlags(Qt::NoItemFlags);
+	this->m_playListModel->appendRow(item);
+
+}
+
+void SPlay::on_playListBtn_clicked(){
+	//qDebug() << __FUNCTION__;
+	ui->playListWidget->isHidden() ? ui->playListWidget->show() : 
+		ui->playListWidget->hide();
+}
+
+void SPlay::on_addPlayListBtn_clicked(){
+	auto filter = "mp4 (*.mp4);;flv (*.flv);;wmv (*.wmv);;\
+		video (*.mp4 *.flv *.wmv);;all (*.*)";
+	auto fileNames = QFileDialog::getOpenFileNames(this, "选择文件",
+		QDir::currentPath(), filter);
+	if (fileNames.isEmpty())return;
+	for (auto& file : fileNames) {
+		auto item = new QStandardItem(QFileInfo(file).fileName());
+		item->setData(file, Qt::UserRole);
+		this->m_playListModel->appendRow(item);
+	}
 }
--- SPlay.h
+++ SPlay.h
@@ -2,20 +2,51 @@
 #define SPLAY_H
 
 #include <QWidget>
+#include<QStandardItemModel>
+#include<QListView>
+#include<QMouseEvent>
+#include<QStyledItemDelegate>
+
+class CustomDelegate : public QStyledItemDelegate {
+public:
+    CustomDelegate(QObject* parent = nullptr) : QStyledItemDelegate(parent) {}
+
+    void paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const override {
+        QStyleOptionViewItem modifiedOption = option;
+        if (index.row() == 0) {
+            modifiedOption.rect.adjust(0, 20, 0, 0); // 向下偏移20像素
+        }
+        QStyledItemDelegate::paint(painter, modifiedOption, index);
+    }
+
+    QSize sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const override {
+        QSize size = QStyledItemDelegate::sizeHint(option, index);
+        if (index.row() == 0) {
+            size.setHeight(size.height() + 20); // 增加高度以适应偏移
+        }
+        return size;
+    }
+};
+
 
 namespace Ui { class SPlay; }
 
 class SPlay : public QWidget
 {
 	Q_OBJECT
-
 public:
 	SPlay(QWidget *parent = nullptr);
 	~SPlay();
 
 	void initUi();
 
+public slots:
+	void on_playListBtn_clicked();
+	void on_addPlayListBtn_clicked();
+
 private:
 	Ui::SPlay* ui{};
+	QStandardItemModel* m_playListModel;
+    CustomDelegate* delegate;
 };
 #endif SPLAY_H
