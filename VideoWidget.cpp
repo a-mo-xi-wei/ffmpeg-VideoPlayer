10392021af1a7f3a881351b6748d944495aecba0
weisang <1428206861@qq.com>
2024-08-08 21:59:15 +0800

    添加开场动画

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -1,7 +1,9 @@
-#include "VideoWidget.h"
+﻿#include"VideoWidget.h"
+#include"SAudioPlay.h"
 #include"SFFmpeg.h"
 #include"SLogger.h"
 #include<QPainter>
+std::once_flag flag;
 VideoWidget::VideoWidget(QWidget *parent)
 	: QWidget(parent)
 {
@@ -15,15 +17,26 @@ VideoWidget::~VideoWidget()
 	this->m_videoThread.stop();
 }
 
-void VideoWidget::threadStart()
+void VideoWidget::PlayBeginVideo()
 {
+	//qDebug() << "width : " << this->width() << " height ：" << this->height();
+	SAudioPlay::instance()->setAudioFormat(SFFmpeg::instance().audioFormat());
+	if (!SAudioPlay::instance()->start())return;
+	std::string path = __FILE__;
+	path += "/../Res/beginVideo.mp4";
+	if (!SFFmpeg::instance().open(path)) {
+		LOG_ERROR("文件打开失败");
+	}
+	SFFmpeg::instance().setPlay(true);
+
 	this->m_videoThread.start();
 }
-
 
 void VideoWidget::paintEvent(QPaintEvent * ev)
 {
 	static int w = 0, h = 0;
+	//qDebug() << "width : " << this->width() << " height ：" << this->height();
+	std::call_once(flag, &VideoWidget::PlayBeginVideo,this);
 	//分配每一帧缓冲区
 	if (w != width() || h != height()) {
 		w = width();
@@ -35,11 +48,10 @@ void VideoWidget::paintEvent(QPaintEvent * ev)
 	QPainter painter(this);
 	auto x = (width() - w) / 2;
 	auto y = (height() - h) / 2;
-	painter.drawImage(QPoint(x,y),*this->m_image);
+	painter.drawImage(QPoint(x, y), *this->m_image);
 }
 
 void VideoWidget::timerEvent(QTimerEvent* ev)
 {
-
 	update();
 }
